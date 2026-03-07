c00f6025170cead3a42a938eec6ac36693abc227
charbel-dib <charbel.dib@ensta.fr>
2026-03-07 14:01:22 +0100

    Add files via upload

---
+++ ENSTA_DATASET/00_config/tracks.py
@@ -0,0 +1,15 @@
+# ENSTA_DATASET/config/tracks.py
+
+TRACKS = {
+    "HYO": "Hydrographie et océanographie",
+    "SOIA": "Systèmes d’observation et intelligence artificielle",
+    "CSN": "Conception de systèmes numériques",
+    "ROB": "Robotique autonome",
+    "ANO": "Architecture navale et offshore",
+    "AV": "Architecture des véhicules",
+    "MAMS": "Modélisation avancée des matériaux et structures",
+    "SP": "Systèmes pyrotechniques",
+    "ISE": "Ingénierie et sciences de l'entreprise",
+    "SHS": "sciences humaines et sociales",
+    "1A-TC-SHS": "sciences humaines et sociales pour tronc commun"
+}
